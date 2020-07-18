115238b19558e0a70f5d31b7607c32e02e73c9a0
AlexLMCode <alexlm0212@gmail.com>
2020-07-17 19:13:30 -0500

    Firts commit, ready the mobile version

---
+++ index.js
@@ -0,0 +1,13 @@
+const menu = document.getElementById('menu');
+const menuBtn = document.getElementById('menuBtn');
+
+
+menuBtn.addEventListener('click', (event) => {
+
+    if (event.target.attributes[0].nodeValue === "./images/icon-hamburger.svg") {
+        event.target.attributes[0].nodeValue = "./images/icon-close.svg";
+    } else {
+        event.target.attributes[0].nodeValue = "./images/icon-hamburger.svg"
+    }
+    menu.classList.toggle("toggle");
+})
